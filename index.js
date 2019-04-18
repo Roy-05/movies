c960cf2aeb04e33241f032ba15801a19af431e59
Saket Roy <theonlyroy@sakets-mbp.attlocal.net>
2019-04-18 16:09:14 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var search_button = document.getElementById('search-button'),
+    list = document.getElementById('list');
+
+search_button.addEventListener('click',()=>{
+    list.innerHTML += "Movie";
+});
