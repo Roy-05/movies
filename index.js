710bd3ecd1e2245217cdd0d9fadba48d1b77dfc9
Saket Roy <theonlyroy@sakets-mbp.home>
2019-04-19 23:18:56 -0500

    Load local resources from filesystem

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var search_button = document.getElementById('search-button'),
     input_box = document.getElementById('input-box'),
     video = document.getElementById('video');
 
+
 var source = document.createElement('source');
 
 search_button.addEventListener('click',()=>{
-
-    source.setAttribute('src', `../My Movies/${input_box.value}.mp4`);
+    let file_name = `../My Movies/${input_box.value}.mp4`;
+    source.setAttribute('src', file_name);
     video.appendChild(source);
-    //video.play();
 });
+
